bcd5ed00497765fe6f6ea4431323877916814580
Fabian Phillipe <fabian.weidner@grapes-app.com>
2024-08-12 20:55:30 +0200

    imlement dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider theme={theme}>
+      <ColorModeScript
+        initialColorMode={theme.config.initialColorMode}
+      />
       <App />
     </ChakraProvider>
   </StrictMode>
