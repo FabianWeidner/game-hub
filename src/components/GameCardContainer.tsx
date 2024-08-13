f63c707fdeffbb509a288bf02ea9740d0c9d878e
Fabian Phillipe <fabian.weidner@grapes-app.com>
2024-08-13 22:06:21 +0200

    rafactor: remove duplicated styles

---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+type GameCardProps = {
+  children: ReactNode;
+};
+
+const GameCardContainer = ({ children }: GameCardProps) => {
+  return (
+    <Box width="300px" borderRadius={10} overflow="hidden">
+      {children}
+    </Box>
+  );
+};
+export default GameCardContainer;
