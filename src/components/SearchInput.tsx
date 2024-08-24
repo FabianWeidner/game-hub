01ab6ea4d36f2c5b160d965cf7edfcdb1129d1ed
Fabian Phillipe <fabian.weidner@grapes-app.com>
2024-08-24 21:03:51 +0200

    implment search

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -3,18 +3,33 @@ import {
   InputGroup,
   InputLeftElement,
 } from "@chakra-ui/react";
+import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
-const SearchInput = () => {
+type SearchInputProps = {
+  onSearch: (searchText: string) => void;
+};
+
+const SearchInput = ({ onSearch }: SearchInputProps) => {
+  const ref = useRef<HTMLInputElement>(null);
+
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />} />
-      <Input
-        borderRadius={20}
-        placeholder="Search games..."
-        variant="filled"
-      />
-    </InputGroup>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current) onSearch(ref.current.value);
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={ref}
+          borderRadius={20}
+          placeholder="Search games..."
+          variant="filled"
+        />
+      </InputGroup>
+    </form>
   );
 };
 export default SearchInput;
