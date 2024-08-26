955213633d861794b3741904f58cd825dca85859
Fabian Phillipe <fabian.weidner@grapes-app.com>
2024-08-26 22:24:26 +0200

    remove react-query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,6 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -20,7 +19,6 @@ createRoot(document.getElementById("root")!).render(
       />
       <QueryClientProvider client={queryClient}>
         <App />
-        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </ChakraProvider>
   </StrictMode>
