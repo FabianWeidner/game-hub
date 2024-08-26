7f0bfa588815e7253e80448dcac72f0b0081bb1a
Fabian Phillipe <fabian.weidner@grapes-app.com>
2024-08-26 21:41:16 +0200

    improve platforms data fetching with react-query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
 export type Platform = {
   id: number;
@@ -6,10 +9,18 @@ export type Platform = {
   slug: string;
 };
 
-const usePlatforms = () => ({
-  data: platforms,
-  error: null,
-  isLoading: null,
-});
+const usePlatforms = () => {
+  const fetchPlatforms = () =>
+    apiClient
+      .get<FetchResponse<Platform>>("/platforms")
+      .then((res) => res.data);
+
+  return useQuery({
+    queryKey: ["platforms"],
+    queryFn: fetchPlatforms,
+    staleTime: 24 * 60 * 60 * 1000, //24,
+    initialData: { count: platforms.length, results: platforms },
+  });
+};
 
 export default usePlatforms;
