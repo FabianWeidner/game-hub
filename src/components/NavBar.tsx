ed67fee36ca96ff79d5be55dc801c587e75261d0
Fabian Phillipe <fabian.weidner@grapes-app.com>
2024-09-14 20:17:55 +0200

    feat(components): implement navigation bar and search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,19 @@
-import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
-import ColorModeSwitch from "./ColorModeSwitch";
-import SearchInput from "./SearchInput";
+import { HStack, Image } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.webp';
+import ColorModeSwitch from './ColorModeSwitch';
+import SearchInput from './SearchInput';
 
-type SearchInputProps = {
-  onSearch: (searchText: string) => void;
-};
-
-const NavBar = ({ onSearch }: SearchInputProps) => {
+const NavBar = () => {
   return (
     <HStack padding="10px">
-      <Image borderRadius="md" src={logo} boxSize="60px" />
-      <SearchInput onSearch={onSearch} />
+      <Link to="/">
+        <Image src={logo} boxSize="60px" objectFit='cover' />
+      </Link>
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
 };
+
 export default NavBar;
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,35 +1,28 @@
-import {
-  Input,
-  InputGroup,
-  InputLeftElement,
-} from "@chakra-ui/react";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import useGameQueryStore from "../store";
 
-type SearchInputProps = {
-  onSearch: (searchText: string) => void;
-};
-
-const SearchInput = ({ onSearch }: SearchInputProps) => {
+const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
+  const setSearchText = useGameQueryStore(s => s.setSearchText);
+  const navigate = useNavigate();
 
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        if (ref.current) onSearch(ref.current.value);
-      }}
-    >
+    <form onSubmit={(event) => {
+      event.preventDefault();
+      if (ref.current) {
+        setSearchText(ref.current.value);
+        navigate('/');
+      }
+    }}>
       <InputGroup>
         <InputLeftElement children={<BsSearch />} />
-        <Input
-          ref={ref}
-          borderRadius={20}
-          placeholder="Search games..."
-          variant="filled"
-        />
+        <Input ref={ref} borderRadius={20} placeholder="Search games..." variant="filled" />
       </InputGroup>
     </form>
   );
 };
+
 export default SearchInput;
