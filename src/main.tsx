afaff8536ae062fdf84cafc7242dcfce7e4d93e9
Fabian Phillipe <fabian.weidner@grapes-app.com>
2024-09-14 20:19:12 +0200

    chore: update main application entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,28 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import theme from "./theme.ts";
 import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import theme from "./theme";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+).render(
+  <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript
         initialColorMode={theme.config.initialColorMode}
       />
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </ChakraProvider>
-  </StrictMode>
+  </React.StrictMode>
 );
