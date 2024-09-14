a114c68a508838fc01996ee73b233e326af0e7a5
Fabian Phillipe <fabian.weidner@grapes-app.com>
2024-09-14 20:18:34 +0200

    feat(services): set up API client and image URL service

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,15 +1,15 @@
-import axios, { AxiosRequestConfig } from "axios";
-const apiKey = import.meta.env.VITE_RAWG_API_KEY;
+import axios, { AxiosRequestConfig } from 'axios';
 
 export interface FetchResponse<T> {
   count: number;
+  next: string | null;
   results: T[];
 }
 
 const axiosInstance = axios.create({
-  baseURL: "https://api.rawg.io/api",
+  baseURL: 'https://api.rawg.io/api',
   params: {
-    key: apiKey,
+    key: 'e53bba19d7914970889f528d70c8e06d',
   },
 });
 
@@ -23,6 +23,12 @@ class APIClient<T> {
   getAll = (config: AxiosRequestConfig) => {
     return axiosInstance
       .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + '/' + id)
       .then((res) => res.data);
   };
 }
