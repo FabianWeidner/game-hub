de7a7e17cc6cb1e94b4dee555422dc8cc6c2cdbe
Fabian Phillipe <fabian.weidner@grapes-app.com>
2024-10-05 04:08:22 +0200

    refactor(ui): hide logo on small screens

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,17 @@
-import { HStack, Image } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
-import logo from '../assets/logo.webp';
-import ColorModeSwitch from './ColorModeSwitch';
-import SearchInput from './SearchInput';
+import { HStack, Image, Box } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
     <HStack padding="10px">
-      <Link to="/">
-        <Image src={logo} boxSize="60px" objectFit='cover' />
-      </Link>
+      <Box display={{ base: "none", md: "block" }}>
+        <Link to="/">
+          <Image src={logo} boxSize="60px" objectFit="cover" />
+        </Link>
+      </Box>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
